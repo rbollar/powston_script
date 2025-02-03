11574a696d588618b6a39fa6ca51298bd62395ea
Ian Connor <iconnor@projectlounge.com>
2025-02-03 16:08:37 +1000

    Add explanation for various sell and SOC points

--- vic_script.py
+++ vic_script.py
@@ -3,7 +3,16 @@ desired_soc = 20.0
 sell_price_threshold = 85  # 20 cents
 sell_price_threshold_1 = 20  # sell in morning
 sell_price_threshold_2 = 1000  # Sell during peak
-buy_price_morning = 5  # Morning buy price
+buy_price_morning = 2  # Morning buy price
+# Low battery SOC is 10% and low sell price is 15 cents
+low_battery_soc = 10
+morning_low_sell_price = 15
+# Morning battery SOC is 20% and morning sell price is 20 cents
+morning_battery_soc = 20
+morning_sell_price = 20
+# High battery SOC is 80% and high sell price is 25 cents
+high_battery_soc = 80
+high_min_sell_price = 25
 
 if 0 <= hour < 6:
     if sell_price > sell_price_threshold:
@@ -31,7 +40,7 @@ if 13 <= hour < 15:
     if sell_price > sell_price_threshold_2:
         action = 'export'
         reason = 'vic: sell price greater than 20 cents between 6am and 1pm'
-    elif buy_price < 20:
+    elif buy_price < 20 and battery_soc < 100:
         action = 'import'
         reason = 'vic: buy price less than 25 cents between 1pm and 3pm'
     else:
@@ -50,25 +59,41 @@ if 21 <= hour < 24:
     if sell_price > sell_price_threshold:
         action = 'export'
         reason = 'vic: sell price greater than 20 cents between 9pm and midnight'
-    elif buy_price < 10:
+    elif buy_price < 10 and battery_soc < 100:
         action = 'import'
         reason = 'vic: buy price less than 5 cents between 9pm and midnight'
     else:
         action = 'auto'
         reason = 'vic: default to auto mode between 9pm and midnight'
 
-if (hour > 15 or hour < 4) and battery_soc > 80 and sell_price > 25:
+if (hour > 15 or hour < 4) and battery_soc > high_battery_soc and sell_price > high_min_sell_price:
     action = 'export'
-    reason = 'vic: use it or lose it'
+    reason = f'vic: use it or lose it down to {high_battery_soc}%'
 
-if (hour < 4) and battery_soc > 20 and sell_price > 20:
+if (hour < 4) and battery_soc > morning_battery_soc and sell_price > morning_sell_price:
     action = 'export'
-    reason = 'vic: use it or lose it'
+    reason = f'vic: morning use it or lose it down to {morning_battery_soc}%'
 
-if (3 < hour < 6) and battery_soc > 10 and sell_price > 15:
+if (3 < hour < 6) and battery_soc > low_battery_soc and sell_price > morning_low_sell_price:
     action = 'export'
-    reason = 'vic: use it or lose it'
+    reason = f'vic: morning use it or lose it down to {low_battery_soc}%'
 
 if rrp > 800 and battery_soc > 30:
     action = 'export'
     reason += 'take the money'
+
+if (interval_time.hour > 15) and battery_soc > 50 and sell_price > 20:
+    best_upcoming = max(sell_forecast)
+    if best_upcoming < (sell_price + 10):
+        action = 'export'
+        reason = f'vic: {best_upcoming} < sell within 10c of max'
+    else:
+        reason += f' best upcoming: {best_upcoming}c'
+
+if (interval_time.hour > 15) and battery_soc > 80 and sell_price > 10:
+    best_upcoming = max(sell_forecast)
+    if best_upcoming < (sell_price + 5):
+        action = 'export'
+        reason = f'vic: {best_upcoming} < sell within 5c of max'
+    else:
+        reason += f' best upcoming: {best_upcoming}c'
